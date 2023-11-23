1edc782bf76062654841c7b8e56dc34ec6d061c1
ssqtmmhp <gongsen@piesat.cn>
2023-11-23 14:53:52 +0800

    first commit

---
+++ src/main/java/com/go/toolbox/ToolboxApplication.java
@@ -0,0 +1,13 @@
+package com.go.toolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolboxApplication.class, args);
+    }
+
+}
